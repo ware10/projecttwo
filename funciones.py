70c26dfb7aa0ff809b040acfd67e199935806994
Francisco Martín Guareño <francisco@MacBook-Pro-de-Francisco-2.local>
2016-12-21 20:11:13 +0100

    Asi empezamos

---
+++ funciones.py
@@ -0,0 +1,10 @@
+def antique_pinch(grado):
+	if grado < 5:
+		print("No pincha ni cristo")
+	elif grado>5 and grado<25:
+		print("aqui pinchamos tos")
+	else:
+		print("hace demasia calo pa ponernos al lio")
+
+def otrafun(x):
+	return("Esta no va a hacer na")
